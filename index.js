066134e13d071a89aeb356dd7a2dc1d86d6875ce
RillMics <kokopokan@gmail.com>
2024-11-10 14:50:33 +0300

    s

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import fs from "node:fs"
 console.log('Happy developing ✨')
 const server = http.createServer(function (request, response){
     fs.readFile("main.html", (_, data) => response.end(data))
-    console.log(request)
-
+    console.log(request.url)
 })
 server.listen(3000, function (){console.log('Listening on port 3000')})
