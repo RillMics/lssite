e8e7704c60b6c462e77ddb9712ffc3a5f828ddc3
RillMics <kokopokan@gmail.com>
2024-11-10 15:07:37 +0300

    s

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ console.log('Happy developing ✨')
 const server = http.createServer(function (request, response){
     fs.readFile("main.html", (_, data) => response.end(data))
     console.log(request)
-
+    console.log("hello tst")
 })
 server.listen(3000, function (){console.log('Listening on port 3000')})
